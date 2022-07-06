c6ddc869fbb9b45e35d03726bb1327c20e6fe4e2
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-06 09:34:56 +0200

    Initial commit of whitespace_strings.py

---
+++ whitespace_strings.py
@@ -0,0 +1,33 @@
+# whitespace refers to a non-printing character
+# e.g. spaces, tebs and end of line symbols
+
+# to add a tab
+print("\tPython")
+
+print("---------------------------------")
+
+# to add a newline
+print("Languages:\nPython\nC\nJavascript")
+
+print("---------------------------------")
+
+# a tab and a new line
+print("Languages:\n\tJavascript\n\tC\n\tPython")
+
+print("---------------------------------")
+# stripping whitespeace e.g "python " to "python"
+# rstrip() method is used to remove a whitespace on the right side of the string
+# lstrip() method is used to remove a whitespace on the left side of the string
+# strip() method is used remove a whitespace on both sides of the string
+
+favourite_language = "python "
+favourite_language = favourite_language.rstrip()
+print(favourite_language)
+
+favourite_language = " C"
+favourite_language = favourite_language.lstrip()
+print(favourite_language)
+
+favourite_language = " JavaScript "
+favourite_language = favourite_language.strip()
+print(favourite_language)
