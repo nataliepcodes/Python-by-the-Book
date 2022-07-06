83a56e9c85007dd8ae2c703032d1670c46df15e1
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-06 14:32:19 +0200

    Initial commit of numbers_exercises.py

---
+++ numbers_exercises.py
@@ -0,0 +1,27 @@
+# 2-8 number eight: write addition, subtraction, multiplication and division operations that result in number 8
+
+addition = 5 + 3
+print(addition) # 8
+
+subtraction = 11 - 3
+print(subtraction) # 8
+
+multiplication = 4 * 2
+print(multiplication) # 8
+
+division = 16 / 2
+print(division) # 8.0 - when you divide to integers you get a float
+
+# 2-9 favourite number: use a variable to represent your favourite number
+# then using that variable create a message that reveals your favourite number
+# print the message
+favourite_number = 9
+message = f"My favourite number is {favourite_number}"
+print(message) # My favourite number is 9
+
+# extra: exponential
+a = 2
+b = 9
+exponential = a ** b
+print(exponential) # 512
+
