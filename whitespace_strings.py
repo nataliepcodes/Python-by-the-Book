dd3525005f46c263a768977648a73177d40538ec
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-06 09:40:24 +0200

    Fixed typing mistakes inside the comments sections

--- whitespace_strings.py
+++ whitespace_strings.py
@@ -1,5 +1,5 @@
 # whitespace refers to a non-printing character
-# e.g. spaces, tebs and end of line symbols
+# e.g. spaces, tabs and end of line symbols
 
 # to add a tab
 print("\tPython")
@@ -11,14 +11,14 @@ print("Languages:\nPython\nC\nJavascript")
 
 print("---------------------------------")
 
-# a tab and a new line
+# to a tab and a new line
 print("Languages:\n\tJavascript\n\tC\n\tPython")
 
 print("---------------------------------")
-# stripping whitespeace e.g "python " to "python"
+# stripping whitespace e.g "python " to "python"
 # rstrip() method is used to remove a whitespace on the right side of the string
 # lstrip() method is used to remove a whitespace on the left side of the string
-# strip() method is used remove a whitespace on both sides of the string
+# strip() method is used to remove a whitespace on both sides of the string
 
 favourite_language = "python "
 favourite_language = favourite_language.rstrip()
