1edefa9289e2fadb1dd20387fdd151f868cac0d4
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-08 13:35:21 +0200

    Initial commit

---
+++ looping_through_list.py
@@ -0,0 +1,20 @@
+seasons = ['winter', 'spring', 'summer', 'autumn']
+for season in seasons:
+    # this is an inside of the loop, executed once for each element in the loop
+    print(season.title())
+    print(f"{season.title()} is a great season!")
+    print(f"Welcome to the {season} season.\n")
+print("Fall is also a season!")
+
+print("\n-----> Exercise 4-1 <-----\n")
+# 4-1 pizzas: identify 3 types of pizzas, print a sentence for each type, at the end print you love pizza
+pizzas = ['pepperoni', 'margherita', 'vegetarian']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza!\n")
+print("I really like pizza!")
+
+print("\n-----> Exercise 4-2 <-----\n")
+animals = ['rhino', 'gorilla', 'turtle']
+for animal in animals:
+    print(f"In 2022 {animal} is an endangered animal.\n")
+print("According to earth.org the above animals are in the top 10 of the World's Most Endangered Animals in 2022.\n")
