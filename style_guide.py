0ed83cd38501b657bf0a60caaf0037dbc8696793
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-09 22:38:41 +0200

    Initial commit

---
+++ style_guide.py
@@ -0,0 +1,5 @@
+print("\n-----> Style Guide: PEP 8 <-----\n")
+# indentation: 4 spaces 
+# line lengths: less than 80 characters; for comments up to 72 chars per line
+# blank lines: visually group parts of the program
+# https://peps.python.org/pep-0008/
