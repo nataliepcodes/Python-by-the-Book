254dad1e71bd01cbf35e8b5ea0c19e6e85f19589
npeyre <90184564+npeyre@users.noreply.github.com>
2022-07-10 12:01:15 +0200

    Initial commit

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# allows to store related information, a collection of key-value pairs
+# below dictionary stores alien's color and point value
+alien_2 = {'color':'yellow', 'points': 10}
+print(alien_2['color'])
+print(alien_2['points'])
